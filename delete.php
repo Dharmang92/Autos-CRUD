0985f0a450f2e7283c3d121b30bee1a52a771750
Dharmang92 <dharmang.gajjar92@gmail.com>
2020-07-27 12:29:16 +0530

    Assignment complete with grade 1. Modified delete.php - working

--- delete.php
+++ delete.php
@@ -3,9 +3,9 @@ require_once "pdo.php";
 session_start();
 
 if (isset($_POST['delete']) && isset($_POST['autos_id'])) {
-    $sql = "DELETE FROM autos WHERE autos_id = :zip";
+    $sql = "DELETE FROM autos WHERE autos_id = :id";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(':zip' => $_POST['autos_id']));
+    $stmt->execute(array(':id' => $_POST['autos_id']));
     $_SESSION['success'] = 'Record deleted';
     header('Location: index.php');
     return;
@@ -17,8 +17,8 @@ if (!isset($_GET['autos_id'])) {
     return;
 }
 
-$stmt = $pdo->prepare("SELECT * FROM autos where autos_id = :xyz");
-$stmt->execute(array(":xyz" => $_GET['autos_id']));
+$stmt = $pdo->prepare("SELECT * FROM autos where autos_id = :id");
+$stmt->execute(array(":id" => $_GET['autos_id']));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if ($row === false) {
     $_SESSION['error'] = 'Bad value for user_id';
@@ -38,7 +38,7 @@ if ($row === false) {
     <div class="container">
         Confirm: Deleting <?= htmlentities($row["make"]); ?>
         <br>
-        <form action="post">
+        <form method="post">
             <input type="hidden" name="autos_id" value="<?php echo $_GET["autos_id"]; ?>">
             <input type="submit" name="delete" value="Delete" />
             <a href="index.php">Cancel</a>
